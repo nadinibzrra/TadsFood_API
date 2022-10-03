8352202fd50386fa4b2f98caae286b3d91a92b60
nadinibzrra <clarinha.nadini@gmail.com>
2022-10-03 11:45:58 -0300

    rota /ofertas

---
+++ src/main/java/ufrn/eaj/tadsfood_api/TadsFoodApiApplication.java
@@ -0,0 +1,13 @@
+package ufrn.eaj.tadsfood_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadsFoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadsFoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufrn/eaj/tadsfood_api/TadsFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.eaj.tadsfood_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadsFoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
